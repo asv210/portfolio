576d77d980a562cab8a96ac662a45597ea526f2c
APAR SOLANKI <solankiapar@gmail.com>
2022-07-15 14:17:34 +0530

    updated

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -4,33 +4,31 @@ const Aboutme = () => {
   return (
     <>
       <div className="aboutm">
-        <div>
-          <div className="content">
-            <ul className="kkkl">
-              <li className="hop">
-                My Name is Apar Vipulkumar Solanki From Una
-              </li>
-              <li className="cur">
-                Currently I'm studing Information Technology in Dharmsinh Desai
-                University
-              </li>
-              <li className="ilik">
-                I Like To Exploar New Things In Technology, Business And Travel.
-              </li>
-              <li className="buid">
-                Currently Building Websites Using HTML , CSS , JS , REACTJS ,
-                BOOTSTRAP
-              </li>
-              <li className="fou">
-                Focusing on Strong my DSA Concepts and Practicing on Various
-                Platforms
-              </li>
-              <li className="flu">
-                Currently Learning Devops , Flutter , Backend of Website
-              </li>
-              <li className="coll">Focused on College Subjects</li>
-            </ul>
-          </div>
+        <div className="content">
+          <ul className="kkkl">
+            <li className="hop">
+              My Name is <br /> Apar Vipulkumar Solanki <br /> From Una
+            </li>
+            <li className="cur">
+              Currently I'm studing Information Technology in Dharmsinh Desai
+              University
+            </li>
+            <li className="ilik">
+              I Like To Exploar New Things In Technology, Business And Travel.
+            </li>
+            <li className="buid">
+              Currently Building Websites Using HTML , CSS , JS , REACTJS ,
+              BOOTSTRAP
+            </li>
+            <li className="fou">
+              Focusing on Strong my DSA Concepts and Practicing on Various
+              Platforms
+            </li>
+            <li className="flu">
+              Currently Learning Devops , Flutter , Backend of Website
+            </li>
+            <li className="coll">Focused on College Subjects</li>
+          </ul>
         </div>
       </div>
     </>

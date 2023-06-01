14f8e60f23a264f2d3fe966b656741358919fa4f
APAR SOLANKI <solankiapar@gmail.com>
2023-06-01 16:23:42 +0530

    about me added

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -7,27 +7,32 @@ const Aboutme = () => {
         <div className="content">
           <ul className="kkkl">
             <li className="hop">
-              My Name is <br /> Apar Vipulkumar Solanki <br /> From Una
+              My Name is <br /> Apar Vipulkumar Solanki
             </li>
             <li className="cur">
-              Currently I'm studing Information Technology in Dharmsinh Desai
-              University
+              ~ 🤔 Currently I'm studing Information Technology
+              <br />~ 💬 In Dharmsinh Desai University
             </li>
             <li className="ilik">
-              I Like To Exploar New Things In Technology, Business And Travel.
+              ~ 👀 I’m interested in technology and learning new technical
+              skills. <br />~ 👯 I Like To Explore New Things In Technology,
+              Business And Travel.
             </li>
-            <li className="buid">
-              Currently Building Websites Using HTML , CSS , JS , REACTJS ,
-              BOOTSTRAP
-            </li>
+
             <li className="fou">
-              Focusing on Strong my DSA Concepts and Practicing on Various
-              Platforms
+              ~ 🌱 I’m currently improving my competitive programming.
+              <br /> ~ 📫 I'm MERN full stack developer otherthan that i do
+              coding in c++ and also advance and core java. <br /> ~ 🤔 Good
+              grip in OS , DBMS , CCN , OOP concepts. <br /> ~ 😄 Excellent in
+              problem solving at various platforms like leetcode , codechef and
+              hackerrank.
+              <br />~ 👯 Otherthan technical knowledge i am also good at
+              building network <br />~ 🌱 And lead the team as i have
+              participated in 5+ hackathons and won in 3+ out of them at various
+              levels
             </li>
-            <li className="flu">
-              Currently Learning Devops , Flutter , Backend of Website
-            </li>
-            <li className="coll">Focused on College Subjects</li>
+
+            <li className="coll">~ ⚡ Focusing on College Subjects as well</li>
           </ul>
         </div>
       </div>
